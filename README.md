b196e3bd4f8a87adaa497c1a298e72d09a8e1d9e
t-yng <t.yng.greenmonkey@gmail.com>
2025-03-20 16:13:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# IST105 Assignment#9
+This is the repository for IST105 assignment#9
+
+## Develop local
+
+```
+$ python3 -m pip install -r requirements.txt
+$ python3 src/09_openroute_parse_json.py
+```
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+sudo dnf update -y
+echo "Start Install Apache: "
+sudo dnf install httpd -y
+echo ""
+
+echo "Start Install Python: "
+sudo dnf install python3.12 -y
+sudo ln -sf /usr/bin/python3.11 /usr/bin/python3
+python3 --version
+
+if [ -f "requirements.txt" ]; then
+    echo "Installing requirements"
+    python3 -m pip install -r requirements.txt
+fi
+
+echo ""
+echo "Start Install PHP: "
+sudo dnf install php php-cli php-common php-fpm php-mysqlnd -y
+php -v
+echo ""
+
+echo "Start Web Server: "
+sudo systemctl start httpd
+
+sudo cp src/* /var/www/html
+echo "Copied src files to /var/www/html"
